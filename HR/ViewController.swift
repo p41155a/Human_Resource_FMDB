95d79ed7b0a1d14eeb9e6a6d05a851251cba8ba2
p41155a <p41155a@gmail.com>
2019-09-19 15:39:06 +0900

    Initial Commit

---
+++ HR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HR
+//
+//  Created by MC975-107 on 19/09/2019.
+//  Copyright © 2019 comso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
